1766d09977c949b7a35d6f07b0dd1f39d41c2e69
BrightBulb123 <83759628+BrightBulb123@users.noreply.github.com>
2021-10-20 15:59:35 +1100

    Added a justification changer tha tasks which justification for uneven values to main.py

--- main.py
+++ main.py
@@ -27,7 +27,7 @@ def table_printer(data):
     for x in range(columns):
         temp = [str(y) for y in all_entries[x::columns]]
         maxWidths.append(len(max(temp, key=len)))
-    
+
     # Generating the horizontal lines for the table
     dividorLine = '+'
 
@@ -47,9 +47,12 @@ def table_printer(data):
             separatorLine.remove('\n')
             for segment in separatorLine:
                 temp = str(segment)
-                while len(segment) != (int(maxWidths[separatorLine.index(temp)])+2):
+                lengthWanted = (int(maxWidths[separatorLine.index(temp)])+2)
+                while len(segment) != lengthWanted:
                     segment += ' '
                     segment = ' ' + segment
+
+                    segment = segmentShortener(segment, lengthWanted)
                 separatorLine[separatorLine.index(temp)] = segment
             temp = '|'
             temp += '|'.join(separatorLine) + '|' + '\n'
@@ -59,8 +62,38 @@ def table_printer(data):
 
     return Table(rows, columns, data, table)
 
+def segmentShortener(segment, lengthWanted):
+    if len(segment) > lengthWanted:
+        lOrR = justificationAsker(segment)
+        while len(segment) > lengthWanted:
+            segment = segment[1:] if lOrR in ("left", "l") else segment[0:-1]
+    return segment
+
+def justificationAsker(segment):
+    lOrR = input(f"One of your values' ({segment.strip()}) length is uneven compared to the column it's in. Would you like it more to the left or right? (left, l, right, r)\n").lower().strip()
+    while lOrR not in ("left", "l", "right" , "r"):
+        print("Please enter one of the following values: 'left', 'l', 'right', or 'r' (without the quotation marks).")
+        lOrR = input(f"One of your values' ({segment.strip()}) length is uneven compared to the column it's in. Would you like it more to the left or right? (left, l, right, r)\n").lower().strip()
+    return lOrR
+
+
+print(table_printer([
+                        ['**C1R1**', 'C1R2', 'C1R3'],
+                        ['C2R1', 'C2R2', '*C2R3*'],
+                        ['*C3R1*', 'C3R2', 'C3R3']
+                    ]).data_table)
+
+print("\n\n")
 
 print(table_printer(
-                    [['column 1', 'column 2'],
-                    [12, 23],
-                    ['row1', 'row2']]).data_table)
+    [
+        ["C1R1"]
+    ]
+).data_table)
+
+print(table_printer(
+    [
+        ["C1R1", "C1R2-Uneven"],
+        ["C2R1-Uneven", "C2R2"]
+    ]
+).data_table)
