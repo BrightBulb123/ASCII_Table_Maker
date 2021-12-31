5904c4ded8541d83c84938ad037cfa7fc2561957
BrightBulb123 <83759628+BrightBulb123@users.noreply.github.com>
2021-12-31 16:36:26 +1100

    Removed the function calls

--- main.py
+++ main.py
@@ -78,7 +78,7 @@ def segmentShortener(segment, lengthWanted):
     if len(segment) > lengthWanted:
         lOrR = justificationAsker(segment)
         while len(segment) > lengthWanted:
-            segment = segment[1:] if lOrR in ("left", "l") else segment[0:-1]
+            segment = segment[1:] if lOrR in ("left", "l") else segment[:-1]
     return segment
 
 def justificationAsker(segment):
@@ -87,21 +87,3 @@ def justificationAsker(segment):
         print("Please enter one of the following values: 'left', 'l', 'right', or 'r' (without the quotation marks).")
         lOrR = input(f"One of your values' ({segment.strip()}) length is uneven compared to the column it's in. Would you like it more to the left or right? (left, l, right, r)\n").lower().strip()
     return lOrR
-
-
-print(table_printer(
-    [
-        ["C1R1", "C1R2-Uneven"],
-        ["C2R1-Uneven", "C2R2"]
-    ]
-).data_table)
-
-print("\n\n")
-
-print(table_printer(
-        [
-          ["C1R1", "C2R1", "C3R1"],
-          ["C1R2", "C2R2", "C3R2"],
-          ["C1R3", "C2R3", "C3R3"],
-      ]
-).data_table)
