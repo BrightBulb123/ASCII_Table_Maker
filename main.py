a552d7957b8ef9975f37d488fb3d439e8ccaf4c1
BrightBulb123 <83759628+BrightBulb123@users.noreply.github.com>
2022-05-03 19:53:49 +1000

    Added excel paste to list function + justification default parameter

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 """Generate a nice and neat table out of text"""
 
-class TableClass():
+
+class TableClass:
     def __init__(self, rows: int, columns: int, data: list, data_table: str) -> None:
         self.rows = rows
         self.columns = columns
@@ -8,7 +9,7 @@ class TableClass():
         self.data_table = data_table
 
 
-def table_printer(data: list) -> TableClass:
+def table_printer(data: list, l_or_r_default: str = None) -> TableClass:
     if type(data) != list:
         return TableClass(None, None, data, "Please enter a valid list for the data.")
 
@@ -21,39 +22,43 @@ def table_printer(data: list) -> TableClass:
     dividorLine = dividorLineMaker(maxWidths)
 
     # Generating the actual table
-    table = ''
+    table = ""
     for x in range(rows):
-        table += dividorLine + '\n'
-        separatorLine = '| ' + ' | '.join(str(data[x][y]) for y in range(columns)) + ' |' + '\n'
+        table += dividorLine + "\n"
+        separatorLine = (
+            "| " + " | ".join(str(data[x][y]) for y in range(columns)) + " |" + "\n"
+        )
         if len(separatorLine) != len(dividorLine):
-            separatorLine = separatorLine.split('|')
-            separatorLine.remove('')
-            separatorLine.remove('\n')
+            separatorLine = separatorLine.split("|")
+            separatorLine.remove("")
+            separatorLine.remove("\n")
             for segment in separatorLine:
                 temp = str(segment)
-                lengthWanted = (int(maxWidths[separatorLine.index(temp)])+2)
+                lengthWanted = int(maxWidths[separatorLine.index(temp)]) + 2
                 while len(segment) != lengthWanted:
-                    segment += ' '
-                    segment = ' ' + segment
+                    segment += " "
+                    segment = f" {segment}"
 
-                    segment = segmentShortener(segment, lengthWanted)
+                    segment = segmentShortener(segment, lengthWanted, l_or_r_default)
                 separatorLine[separatorLine.index(temp)] = segment
-            temp = '|'
-            temp += '|'.join(separatorLine) + '|' + '\n'
-            separatorLine = str(temp)
+            temp = "|"
+            temp += "|".join(separatorLine) + "|" + "\n"
+            separatorLine = temp
             table += separatorLine
     table += dividorLine
 
     return TableClass(rows, columns, data, table)
 
+
 def dividorLineMaker(maxWidths):
-    dividorLine = '+'
+    dividorLine = "+"
 
     for lineWidth in maxWidths:
-        for _ in range(lineWidth+2):
-            dividorLine += '-'
-        dividorLine += '+'
+        for _ in range(lineWidth + 2):
+            dividorLine += "-"
+        dividorLine += "+"
     return dividorLine
+
 
 def findMaxWidth(all_entries, columns):
     maxWidths = []
@@ -62,28 +67,54 @@ def findMaxWidth(all_entries, columns):
         maxWidths.append(len(max(temp, key=len)))
     return maxWidths
 
+
 def rowsColumnsAll_entries(data):
     rows = len(data)
 
     # figuring out how many columns there are
     all_entries = []
     for row in data:
-        for item in row:
-            all_entries.append(item)
+        all_entries.extend(iter(row))
+    columns = int(round(len(all_entries) / rows))
+    return rows, all_entries, columns
 
-    columns = int(round(len(all_entries)/rows))
-    return rows,all_entries,columns
 
-def segmentShortener(segment, lengthWanted):
+def segmentShortener(segment, lengthWanted, l_or_r_default=None):
     if len(segment) > lengthWanted:
-        lOrR = justificationAsker(segment)
+        lOrR = justificationAsker(segment) if l_or_r_default is None else l_or_r_default
         while len(segment) > lengthWanted:
             segment = segment[1:] if lOrR in ("left", "l") else segment[:-1]
     return segment
 
+
 def justificationAsker(segment):
-    lOrR = input(f"One of your values' ({segment.strip()}) length is uneven compared to the column it's in. Would you like it more to the left or right? (left, l, right, r)\n").lower().strip()
-    while lOrR not in ("left", "l", "right" , "r"):
-        print("Please enter one of the following values: 'left', 'l', 'right', or 'r' (without the quotation marks).")
-        lOrR = input(f"One of your values' ({segment.strip()}) length is uneven compared to the column it's in. Would you like it more to the left or right? (left, l, right, r)\n").lower().strip()
+    lOrR = (
+        input(
+            f"One of your values' ({segment.strip()}) length is uneven compared to the column it's in. Would you like it more to the left or right? (left, l, right, r)\n"
+        )
+        .lower()
+        .strip()
+    )
+    while lOrR not in ("left", "l", "right", "r"):
+        print(
+            "Please enter one of the following values: 'left', 'l', 'right', or 'r' (without the quotation marks)."
+        )
+        lOrR = (
+            input(
+                f"One of your values' ({segment.strip()}) length is uneven compared to the column it's in. Would you like it more to the left or right? (left, l, right, r)\n"
+            )
+            .lower()
+            .strip()
+        )
     return lOrR
+
+
+def excel_copy_to_list(data: str) -> list[str]:
+    final = []
+    rows = data.split("\n")
+    for row in rows:
+        columns = row.split("\t")
+        temp = list(columns)
+        final.append(temp)
+
+    return final[:-1] if final[-1] == [""] else final
